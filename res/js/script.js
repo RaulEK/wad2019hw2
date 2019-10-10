accf81a5f28bfd44b4b2730a2c1e126e3c567da1
Krister Looga <looga.krister@gmail.com>
2019-10-10 22:09:16 +0300

    Added course trigger logic. Added gradeToPoint function

--- res/js/script.js
+++ res/js/script.js
@@ -1,45 +1,49 @@
-$(function () {
-  var user = new User('Mart', 'Mets', '10/10/2000', 'Software Engineering', 3.5);
-  var courses = [
-    new Course('Web application development', 3, 'F'),
-    new Course('Introduction to data science', 2, 'D'),
-    new Course('Algoritmid ja andmestruktuurid', 3, 'A'),
-    new Course('Sissejuhatus erialasse', 1, 'B')
-  ];
+$(() => {
+    const user = new User('Mart', 'Mets', '10/10/2000', 'Software Engineering', 3.5);
+    let courses = [
+        new Course('Web application development', 3, '40'),
+        new Course('Introduction to data science', 2, '60'),
+        new Course('Algoritmid ja andmestruktuurid', 3, '91'),
+        new Course('Sissejuhatus erialasse', 1, '81')
+    ];
 
-  init();
+    init();
 
-  $("#profile-button").click( function() {
-    $("#courses-button").removeClass("active");
-    $("#profile-button").addClass("active");
-    $("#courses-container").removeClass("active");
-    $("#profile-container").addClass("active")
-  });
+    $("#profile-button").click(() => {
+        $("#courses-button").removeClass("active");
+        $("#profile-button").addClass("active");
+        $("#courses-container").removeClass("active");
+        $("#profile-container").addClass("active")
+    });
 
-  $("#courses-button").click( function() {
-    $("#profile-button").removeClass("active");
-    $("#courses-button").addClass("active");
-    $("#profile-container").removeClass("active");
-    $("#courses-container").addClass("active")
-  });
+    $("#courses-button").click(() => {
+        $("#profile-button").removeClass("active");
+        $("#courses-button").addClass("active");
+        $("#profile-container").removeClass("active");
+        $("#courses-container").addClass("active")
+    });
 
-  function init() {
-    $("#name").text(user.firstname + ' ' + user.lastname);
-    $("#birthdate").text(user.birthdate);
-    $("#faculty").text(user.faculty);
-    $("#gpa").text(user.gpa);
-  }
+    function init() {
+        $("#name").text(user.firstname + ' ' + user.lastname);
+        $("#birthdate").text(user.birthdate);
+        $("#faculty").text(user.faculty);
+        $("#gpa").text(user.gpa);
+    }
 
-  for (let i = 0; i < courses.length; i++) {
-    let tr = $("<tr></tr>");
-    let td1 = $("<td></td>").text(i+1);
-    let td2 = $("<td></td>").text(courses[i].title);
-    let td3 = $("<td></td>").text(courses[i].semester);
-    let td4 = $("<td></td>").text(courses[i].grade);
-    tr.append(td1);
-    tr.append(td2);
-    tr.append(td3);
-    tr.append(td4);
-    $("#course-details").append(tr);
-  }
+    for (let i = 0; i < courses.length; i++) {
+        const tr = $("<tr></tr>");
+        const td1 = $("<td></td>").text(i + 1);
+        const td2 = $("<td></td>").text(courses[i].title);
+        const td3 = $("<td></td>").text(courses[i].semester);
+        const td4 = $("<td></td>").text(courses[i].grade);
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+        $("#course-details").append(tr);
+    }
+
+    $("#add-course-button").click(() => $("#add-course").css({display: $("#add-course").css("display") === "none" ? "inline" : "none"}));
+
+    const gradeToPoint = (grade) => grade > 90 ? 4 : grade > 80 ? 3 : grade > 70 ? 2 : grade > 60 ? 1 : grade > 50 ? 0.5 : 0
 });
